58f2107d8ce35ab3f82a3b977135a5c1feb16e96
Lapin Matvey <lapinmatvej84@gmail.com>
2025-12-02 18:38:18 +0300

    Фикс импорта src для всех тестов

--- courses-service/tests/test_health.py
+++ courses-service/tests/test_health.py
@@ -1,6 +1,13 @@
+import os
+import sys
+
+CURRENT_DIR = os.path.dirname(__file__)
+SERVICE_ROOT = os.path.dirname(CURRENT_DIR)
+if SERVICE_ROOT not in sys.path:
+    sys.path.insert(0, SERVICE_ROOT)
+
 from fastapi.testclient import TestClient
 from src.main import app
-
 
 client = TestClient(app)
 
